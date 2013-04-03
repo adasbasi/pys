932ee80642d271bb1b2af5bbc18bc5a73bea90a7
Ali Daşbaşı <ali.dasbasi@bil.omu.edu.tr>
2013-04-03 14:35:29 +0300

    sqlite3 sil

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Pys::Application.config.secret_token = '9599ed38183a23ec4ff62ae7c9e3c882f20593b292e55ff593a4c3d41e096372a3003aa9ad4bc30065b0e39d4929a198b25ed3a72a79eeb89ed7a783977eb32a'
