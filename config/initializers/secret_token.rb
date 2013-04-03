f15675c9b665f9fd816f87bbd44c35d429bf6274
Ali Daşbaşı <ali.dasbasi@bil.omu.edu.tr>
2013-04-03 14:35:43 +0300

    mysql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pys::Application.config.secret_token = '6384df125ce2a481fe771aff882e7e87f1745887f20c057ab236b809c9bfb244e33666c3f1be2b0c6564f0537b6145a51dc51666a635f70dcce82c3dab32adfe'
